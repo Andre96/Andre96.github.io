d03785ae5b89702bebe5cb0e22b967ac27bcf7a1
Andrea Catena <andreacatena@MBP-di-Andrea.home>
2021-05-14 12:55:18 +0200

    day and night toggle

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function toggle_dn(){
+    var body = document.body;
+    var nav = document.getElementById("navbar");
+    var elements = [body, nav];
+    var i;
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].classList.toggle("dark_mode")
+    }
+    if(body.classList.contains("dark_mode")){
+        document.getElementById("toggle_dn").src = "images/night.jpeg";
+    } else {
+        document.getElementById("toggle_dn").src = "images/light.png";
+    }
+}
